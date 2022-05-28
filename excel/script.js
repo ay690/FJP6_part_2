5ec2c78b37dc235a735036df5f11c8da572bfeb0
Aniket Yadav <aniketyadav690@gmail.com>
2022-05-28 23:21:07 +0530

    blur event added on cells

--- excel/script.js
+++ excel/script.js
@@ -19,8 +19,20 @@ for(let i=0;i<allCells.length;i++){
         let rowId = Number(e.target.getAttribute("rowid"));
         let colId = Number(e.target.getAttribute("colid"));
         let address = String.fromCharCode(65+colId)+(rowId+1)+"";
-         console.log(address);
+        // console.log(address);
         addressInput.value = address;
+    })
+
+    allCells[i].addEventListener("blur",function(e){
+        let cellValue = e.target.textContent;
+        let rowId  = e.target.getAttribute("rowid");
+        let colId = e.target.getAttribute("colid");
+        let cellObject = db[rowId][colId];
+        if(cellObject.value == cellValue){
+            return;
+        }
+        cellObject.value = cellValue;
+        console.log("After UPdate",cellObject);
     })
 }
 

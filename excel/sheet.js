b74b2676486614a08d9ab65d48640df032097e8b
Aniket Yadav <aniketyadav690@gmail.com>
2022-06-05 23:41:47 +0530

    sheets been added

---
+++ excel/sheet.js
@@ -0,0 +1,15 @@
+let addBtn = document.querySelector(".add-sheet");
+let sheetList = document.querySelector(".sheets-list");
+let sheetId = 0;
+
+addBtn.addEventListener("click", function(){
+   sheetId++;
+   let activeSheet = document.querySelector(".active-sheet");
+   activeSheet.classList.remove("active-sheet");
+   let sheetDiv = document.createElement("div");
+   sheetDiv.classList.add("sheet");
+   sheetDiv.classList.add("active-sheet");
+   sheetDiv.setAttribute("sheetId", sheetId);
+   sheetDiv.innerText = `Sheet ${sheetId + 1}`;
+   sheetList.append(sheetDiv);
+})

52923bb0a6f444dc97658ef2ce7c8a206f22c40c
Aniket Yadav <aniketyadav690@gmail.com>
2022-06-21 22:22:23 +0530

    MovieList components added

--- module_4/reactPrerequisite/react/movies-app/src/App.js
+++ module_4/reactPrerequisite/react/movies-app/src/App.js
@@ -1,12 +1,14 @@
 import NavBar from './components/NavBar';
 import './App.css';
 import Banner from './components/Banner';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div>
     <NavBar/>
     <Banner/>
+    <MovieList/>
     </div>
   );
 }
--- module_4/reactPrerequisite/react/movies-app/src/components/Banner.js
+++ module_4/reactPrerequisite/react/movies-app/src/components/Banner.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-
+import {movies} from "../movieData"
 
 class Banner extends Component{
     render(){

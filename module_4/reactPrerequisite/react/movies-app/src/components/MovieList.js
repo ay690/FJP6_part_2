f8be3833b9f4032f8bdb612b4d4da7c1cd18538d
Aniket Yadav <aniketyadav690@gmail.com>
2022-06-23 23:09:52 +0530

    movies from API call and lifecycle method

--- module_4/reactPrerequisite/react/movies-app/src/components/MovieList.js
+++ module_4/reactPrerequisite/react/movies-app/src/components/MovieList.js
@@ -1,38 +1,48 @@
 import { Component } from "react";
-import {movies} from "../movieData"
+// import { movies } from "../movieData"
+import axios from 'axios'
 
-class MovieList extends Component{
+class MovieList extends Component {
     constructor(){
         super();
-        this.state = {
+        this.state={
             hover:"",
-            pArr : [1]
+            pArr :[1],
+            movies:[]
         };
     }
 
-    
-    render(){
-        let moviesArr = movies.results
-        return(
+    async componentDidMount(){
+        console.log("Component Did Mount");
+        const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=81242a2aa2066e052c78ec9ac1700c59&language=en-US&page=1')
+        console.log(res.data);
+        this.setState({
+            movies:[...res.data.results]
+        })
+    }
+
+    render() {
+        console.log("rednered");
+        // let moviesArr = movies.results
+        return (
             <>
-            <div>
-                <h3 className="text-center"><strong>Trending</strong></h3>
-            </div>
-            <div className="movies-list">
-                {moviesArr.map((movieEle)=>(
-                    <div className="card movie-card" onMouseEnter={() => this.setState({hover: movieEle.id})} onMouseLeave={() => this.setState({hover:""})}>
-                        <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{height:'40vh',width:'20vw'}} className="card-img-top movie-img" alt="..."/>
-                        <h5 className="card-title movie-title">{movieEle.title}</h5>
-                        <div style={{ display: 'flex', justifyContent: "center" }}>
+                <div>
+                    <h3 className="text-center"><strong>Trending</strong></h3>
+                </div>
+                <div className="movies-list">
+                    {this.state.movies.map((movieEle) => (
+                        <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})} >
+                            <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+                            <h5 className="card-title movie-title">{movieEle.title}</h5>
+                            <div style={{ display: 'flex', justifyContent: "center" }}>
                                 {this.state.hover == movieEle.id && (
                                 <a href="#" type="button" className="btn btn-primary movies-button">Add to Favourites</a>)}
                             </div>
-                    </div>
-                ))}
-            </div>
-            
-            <div style={{display:"flex",justifyContent:"center"}}>
-            <nav aria-label="Page navigation example">
+                        </div>
+                    ))}
+                </div>
+                <div style={{display:"flex",justifyContent:"center"}}>
+                <nav aria-label="Page navigation example">
                     <ul className="pagination">
                         <li className="page-item"><a className="page-link" href="#">Previous</a></li>
                         {this.state.pArr.map((ele)=>(
@@ -47,6 +57,5 @@ class MovieList extends Component{
         )
     }
 }
-
 
 export default MovieList

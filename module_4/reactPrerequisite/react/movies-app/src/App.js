7bf8f8eb82818d6270359d137beaac596f313eee
Aniket Yadav <aniketyadav690@gmail.com>
2022-06-28 22:02:55 +0530

    Routing implemented

--- module_4/reactPrerequisite/react/movies-app/src/App.js
+++ module_4/reactPrerequisite/react/movies-app/src/App.js
@@ -1,18 +1,19 @@
-import NavBar from './components/NavBar';
 import './App.css';
 import Banner from './components/Banner';
 import MovieList from './components/MovieList';
-import Fav from './components/Fav';
+import NavBar from './components/NavBar';
+import Fav from './components/Fav'
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-    <NavBar/>
-    {/* <Banner/>
-    <MovieList/> */}
-    <Fav/>
-    </div>
-  );
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<><Banner/><MovieList/></>}/>
+        <Route path="/favourites" element={<Fav/>}/>
+      </Routes>
+    </BrowserRouter>
+    );
 }
-
 export default App;

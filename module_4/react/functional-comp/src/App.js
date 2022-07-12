4bb1f227a77961d464f38cfa51ad88da56e16073
Aniket Yadav <aniketyadav690@gmail.com>
2022-07-12 22:51:28 +0530

    Context API and ThemeChanger

--- module_4/react/functional-comp/src/App.js
+++ module_4/react/functional-comp/src/App.js
@@ -6,13 +6,15 @@ import Ue2 from './components/Ue2';
 import Ue3 from './components/Ue3';
 import Parent from './components/Context/Parent';
 import Test from './components/Context/Test';
+import ThemeChange from './components/Context/ThemeChange';
 
 function App() {
   return (
    // <Counter/>
    //<Ue3/>
    //<Parent/>
-   <Test/>
+   //<Test/>
+   <ThemeChange/>
   );
 }
 

8b52bfdda74e413b5999df9b248bc61485f90b2c
Aniket Yadav <aniketyadav690@gmail.com>
2022-07-16 22:32:26 +0530

    Login functionality

--- module_4/react/reel-app/src/firebase.js
+++ module_4/react/reel-app/src/firebase.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCcNFydwvr9f2M10btON3rwndcDaLNC7BI",
   authDomain: "class-demo-e40b9.firebaseapp.com",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export let auth = getAuth(app);

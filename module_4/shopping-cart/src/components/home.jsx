215b32336a1cd89430398edaac364643e57d435b
Aniket Yadav <aniketyadav690@gmail.com>
2022-07-29 21:03:57 +0530

    Home component based on state

--- module_4/shopping-cart/src/components/home.jsx
+++ module_4/shopping-cart/src/components/home.jsx
@@ -1,18 +1,27 @@
 import Product from "./product"
 import "./home.css"
-
-let Home = ()=>{
-    return(
+import {useSelector} from "react-redux"
+//import {useHistory} from "react-router"
+let Home = () => {
+    //let history = useHistory();
+    let state = useSelector((state)=>state);
+    return (
         <>
-        <div className="product-container">
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-
-        </div>
+            <div className="product-container">
+                {state.map((el,index)=>(
+                    <Product key={index} data={el} />
+                ))}
+                
+            </div>
+            <button
+            // onClick={()=>{
+            //     history.push("/cart")
+            // }}
+            className="shopping-cart-home">
+                <span class="material-symbols-outlined">
+                    shopping_cart
+                </span>
+            </button>
         </>
     )
 }

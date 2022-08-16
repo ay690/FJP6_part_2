6384e31b5c1e4ffceb8bf40a3fe3327af06b57b9
Aniket Yadav <aniketyadav690@gmail.com>
2022-08-16 23:31:41 +0530

    cookies added

--- module_5/foodApp/server.js
+++ module_5/foodApp/server.js
@@ -2,7 +2,12 @@ const  express = require("express")
 
 const app = express();
 
+//npm i cookie-parser
+const cookieParser = require("cookie-parser");
+
 app.use(express.json());
+app.use(cookieParser());
+
 
 const userModel = require("./userModel");
 
@@ -34,11 +39,12 @@ app.post("/login",async function(req,res){
         let data = req.body;
         // console.log(data);
         let {email,password} = data;
-        
+
         if(email && password){
             let user = await userModel.findOne({email:email});
             if(user){
                 if(user.password == password){
+                    res.cookie("token","sample value");
                     res.send("User logged in");
                 }else{
                     res.send("Email or Password does not match");
@@ -54,6 +60,12 @@ app.post("/login",async function(req,res){
     }
 })
 
+
+app.get("/users", function(req, res){
+  console.log(req.cookies);
+  res.send("cookie-read successfully");
+  
+})
 
 
 app.listen(3000,function(){
